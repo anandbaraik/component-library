dc24d887edd1992dc5c19b0c8036d79f529d97b9
anandbaraik <anandbaraik2014@gmail.com>
2023-09-13 01:30:08 +0530

    navbar added :zap:

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+    	<p>
+        	OneUI
+      	</p>
+		<form>
+			<input type="search" name="search" id="search"/>
+			<button type="submit">Search</button>
+		</form>
+		<div>
+			<Link to="/">
+				Home
+			</Link>
+			<Link to={'/components'}>
+				Components
+			</Link>
+		</div>
+    </nav>
   )
 }
 
